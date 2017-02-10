ef307ebf41a932b953860014467b5337239d623e
vestor <manish001992@gmail.com>
2017-02-10 10:42:17 +0530

    First commit with initial setup

---
+++ src/main/java/com/judge/OpenSourceJudgeApplication.java
@@ -0,0 +1,12 @@
+package com.judge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenSourceJudgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenSourceJudgeApplication.class, args);
+	}
+}
